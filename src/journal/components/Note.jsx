55a263a30a8b5dc8b246e4047d4968f5af58bbdb
JL-OLEMAR <joseluis19963@gmail.com>
2022-10-06 00:00:35 -0500

    feat: Select files from react

--- src/journal/components/Note.jsx
+++ src/journal/components/Note.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Button, Grid, TextField, Typography } from '@mui/material'
-import { SaveOutlined } from '@mui/icons-material'
+import { Button, Grid, IconButton, TextField, Typography } from '@mui/material'
+import { SaveOutlined, UploadOutlined } from '@mui/icons-material'
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.css'
 
@@ -11,6 +11,7 @@ import { setActiveNote, startSaveNote } from '../../store/journal'
 import { ImageGallery } from './ImageGallery.jsx'
 
 export function Note() {
+  const fileInputRef = useRef()
   const dispatch = useDispatch()
   const {
     active: note,
@@ -43,6 +44,12 @@ export function Note() {
     dispatch(startSaveNote())
   }
 
+  const onFileInputChange = ({ target }) => {
+    if (target.files === 0) return
+    console.log('upload images')
+    // dispatch(startUploadingFiles(target.files))
+  }
+
   return (
     <Grid
       container
@@ -59,6 +66,21 @@ export function Note() {
       </Grid>
 
       <Grid item>
+        <input
+          ref={fileInputRef}
+          multiple
+          style={{ display: 'none' }}
+          type='file'
+          onChange={onFileInputChange}
+        />
+        <IconButton
+          color='primary'
+          disabled={isSaving}
+          onClick={() => fileInputRef.current.click()}
+        >
+          <UploadOutlined />
+        </IconButton>
+
         <Button
           color='primary'
           disabled={isSaving}

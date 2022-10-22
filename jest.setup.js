2cee2bae00de7b7507b68bef84c1c35ad7f09be5
JL-OLEMAR <joseluis19963@gmail.com>
2022-10-22 02:45:03 -0500

    chore: Environment variables for dev and test mode

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,9 @@
 import 'whatwg-fetch'
 import 'setimmediate'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.test.local' })
+
+jest.mock('./src/helpers/getEnvironments.js', () => ({
+  getEnvironments: () => ({ ...process.env })
+}))
